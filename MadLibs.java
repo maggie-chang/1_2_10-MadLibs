497b5653545ca617b6d1c1bd60f80a08affa2d38
maggie.chang27@student.tamdistrict.org <maggie.chang27@student.tamdistrict.org>
2024-09-26 09:30:23 -0700

    madlibscanner & string

--- MadLibs.java
+++ MadLibs.java
@@ -1,4 +1,15 @@
+import java.util.Scanner;
+
 public class MadLibs
 {
-    
+    public static void main(String[] args)
+    {
+    Scanner scanner = new Scanner(System.in);  // Create a Scanner object
+    //String madLib = "Eli is very <adjective>.";
+
+    System.out.println("Enter an adjective: ");
+    String input = scanner.nextLine();
+    System.out.println("you said: "  + input);
+
+    }
 }
