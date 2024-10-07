5079526cf23475945056adf9dcac59fe164bacc6
maggie <144709218+maggie-chang@users.noreply.github.com>
2024-10-07 05:02:49 +0000

    *forgot to commit* first word descriptor done, need the rest of words

--- MadLibs.java
+++ MadLibs.java
@@ -4,12 +4,26 @@ public class MadLibs
 {
     public static void main(String[] args)
     {
-    Scanner scanner = new Scanner(System.in);  // Create a Scanner object
-    //String madLib = "Eli is very <adjective>.";
+    Scanner scanner = new Scanner(System.in); //creates a scanner object
+    String madLib = "<Proper noun> is a <adjective> apple, but a cat <verb> it."; // madLib 1
+    //madLib = ""; // madLib 2
+    //madLib = ""; //madLib 3
 
-    System.out.println("Enter an adjective: ");
+    // finds the index of brackets for word descriptor
+    int indexLeftBracket = madLib.indexOf("<");
+    int indexRightBracket = madLib.indexOf(">");
+    
+    // finds and replaces word and brackets with user input
+    String before = madLib.substring(0, indexLeftBracket);
+    String after = madLib.substring(indexRightBracket + 1);
+
+    System.out.println("Enter a " + madLib.substring(indexLeftBracket, indexRightBracket + 1);
     String input = scanner.nextLine();
-    System.out.println("you said: "  + input);
+    // this will update the madLib
+    madLib = before + input + after;
+    
+
+    System.out.println(madLib);
 
     }
 }
